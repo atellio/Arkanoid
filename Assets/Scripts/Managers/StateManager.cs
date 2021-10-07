38c817087aea7354cda4dc29a44df841bcd4ed3e
Adrian Tosello <adriantosello@gmail.com>
2021-10-07 17:02:45 +1100

    Change brick prefab to have single renderer, add State script and StateManager, GameManager stubb

---
+++ Assets/Scripts/Managers/StateManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateManager : MonoBehaviour
+{
+    private State currentState;
+
+
+    public void SetState(State state)
+    {
+        if (currentState != null)
+        {
+            currentState.OnStateExit();
+        }
+        currentState = state;
+        currentState.OnStateEnter();
+    }
+
+    private void Update()
+    {
+        if (currentState != null) currentState.UpdateState();
+    }
+}
---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class State
+{
+    public abstract void OnStateEnter();
+    public abstract void UpdateState();
+    public abstract void OnStateExit();
+}
